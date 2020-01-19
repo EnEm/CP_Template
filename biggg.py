3e6394078e44540e44f54b69dbf019531817df66
EnEm <46641699+EnEm@users.noreply.github.com>
2020-01-19 18:36:14 +0530

    Changed the python file a bit

--- biggg.py
+++ biggg.py
@@ -1,13 +1,7 @@
 ntc=1
 #ntc=(int)(input())
 for tc in range(1,ntc+1):
-    n=(int)(input())
+    n=int(input())
+    a=list(map(int,input().split()))
 
-    res=input().split()
-    a=[]
-    for i in res:
-        a+=[int(i)]
-
-    print(tc,end=' ')
-    print(a,end=' ')
-    print(n)
+    
